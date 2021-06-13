6f776853cbfa2379cd927badd520090bb51dc86f
Simon Law <simonwhl08@gmail.com>
2021-06-13 14:12:35 +0100

    added fetch requests and file structure

---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+export const signup = async (username, email, password, setUser) => {
+    const response = await fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password
+        }),
+    });
+    const data = await response.json();
+    setUser(data.username)
+}
+
+export const updateUser = async (username, email, password, setUser) => {
+    const response = await fetch('http://localhost:5000/users', {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            username: username,
+            update: {
+                username: username,
+                email: email,
+                password: password
+            }
+        }),
+    });
+    const data = await response.json();
+    setUser(data)
+}
