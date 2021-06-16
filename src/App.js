9a10220c97ff9ef4f97b9878001cd4a48541f34d
simonl08 <62649746+simonl08@users.noreply.github.com>
2021-06-16 10:06:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import React, { useState } from 'react';
+import Home from './pages/Home';
+import { GenericStyle } from './styles/Theme';
+
+const App = () => {
+  const [user, setUser] = useState("");
+
+  return (    
     <div className="App">
       <GenericStyle/>
         <Home setUser={setUser}/>
